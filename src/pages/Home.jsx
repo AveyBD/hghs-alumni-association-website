954810422fe67aaa781c55f43fe3a84de29079cb
Debashish Dev <hello@debashish.dev>
2023-01-31 16:39:22 +0600

    Hero Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Hero from "../components/Home/Hero";
 import Navbar from "../components/Shared/Navbar";
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 };
