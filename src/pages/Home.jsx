dac648795a11121527db3acb29a71ca3c41732cf
Debashish Dev <hello@debashish.dev>
2023-02-01 15:53:06 +0600

    Added Member Card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Hero from "../components/Home/Hero";
+import IconBox from "../components/Home/IconBox";
 import Navbar from "../components/Shared/Navbar";
 
 const Home = () => {
@@ -7,6 +8,11 @@ const Home = () => {
     <>
       <Navbar />
       <Hero />
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-3 px-20">
+        <IconBox />
+        <IconBox />
+        <IconBox />
+      </div>
     </>
   );
 };
